8e9e3ba8bd4a781cc4f896ea4c90a2f2e00255d6
TomerAsraf <tomerasraf2@gmail.com>
2021-09-24 11:13:08 +0300

    First Commit
    Unity version 2019.4.10f1

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] public GameObject cardBack;
+
+    // give access to the SceneController script (class)
+    [SerializeField] private SceneController controller = null;
+
+    // get a read-only of the id
+    private int _id;
+    public int id
+    {
+        get { return _id; }
+    }
+
+    // Give the card id and image of the memory card
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    // if the card back is active and the second card didnt reveal it self yet.
+    void OnMouseDown()
+    {
+        if (cardBack.activeSelf && controller.canReveal)
+        {
+            cardBack.SetActive(false);
+            controller.CardRevealed(this);
+        }
+    }
+
+    public void Unreveal()
+    {
+        cardBack.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public const int gridRows = 2;
+    public const int gridCols = 4;
+    public const float offsetX = 2f;
+    public const float offsetY = 2.5f;
+
+    // refernce to check the card condition
+    private MemoryCard _firstRevealed;
+    private MemoryCard _secondRevealed;
+
+    public TextMesh score = null;
+    private int _score = 0;
+
+    [SerializeField] private MemoryCard originalCard = null;
+    [SerializeField] private Sprite[] images = null;
+
+    // getter with condtion for the memory card script
+    public bool canReveal
+    {
+        get { return _secondRevealed == null; }
+    }
+
+    // take the object form the MemoryCard script into the refernce objects
+    public void CardRevealed(MemoryCard card)
+    {
+        if (_firstRevealed == null)
+        {
+            _firstRevealed = card;
+        }
+        else
+        {
+            _secondRevealed = card;
+            StartCoroutine(CheckMatch());
+        }
+    }
+
+    // suffle the array for more randomnes
+    private int[] suffleArray(int[] numbers)
+    {
+        int[] newArray = numbers.Clone() as int[];
+        for (int i = 0; i < newArray.Length; i++)
+        {
+            int tmp = newArray[i];
+            int r = Random.Range(i, newArray.Length);
+            newArray[i] = newArray[r];
+            newArray[r] = tmp;
+
+        }
+        return newArray;
+    }
+
+    // wait for message from the UIButton script that activite the restart.
+    public void Restart(){
+        SceneManager.LoadScene("Scene");
+    }
+
+
+    void Start()
+    {
+        Vector3 startPos = originalCard.transform.position;
+
+        int[] numbers = { 0, 0, 1, 1, 2, 2, 3, 3 };
+        numbers = suffleArray(numbers);
+
+        // 2x for loops to achive the grid of cards
+        for (int i = 0; i < gridCols; i++)
+        {
+            for (int j = 0; j < gridRows; j++)
+            {
+                MemoryCard card;
+                if (i == 0 && j == 0)
+                {
+                    card = originalCard;
+                }
+                else
+                {   
+                    card = Instantiate(originalCard) as MemoryCard;
+                }
+
+                int index = j * gridCols + i; // another small suffel based on the loop
+                int id = numbers[index];
+                card.SetCard(id, images[id]);
+
+                float posX = (offsetX * i) + startPos.x;
+                float posY = -(offsetY * j) + startPos.y;
+
+                card.transform.position = new Vector3(posX, posY, startPos.z);
+            }
+        }
+
+
+    }
+
+    // corutine that responseble on the score, unreveal cards that dont mach and making the object refrence null again for another use.
+    private IEnumerator CheckMatch()
+    {
+        if (_firstRevealed.id == _secondRevealed.id)
+        {
+            _score++;
+            score.text = "Score:" + _score;
+        }
+        else
+        {
+            yield return new WaitForSeconds(.5f);
+
+            _firstRevealed.Unreveal();
+            _secondRevealed.Unreveal();
+        }
+
+        _firstRevealed = null;
+        _secondRevealed = null;
+    }
+
+}
